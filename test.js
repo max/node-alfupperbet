95a4d5b0242316b3829a42f340e760788f812d0c
Max Schoening <mail@maximilian.io>
2015-01-21 18:46:55 -0800

    WTF? Tabs? What are we? Animals?

--- test.js
+++ test.js
@@ -5,6 +5,6 @@ var alfupperbet = require('./');
 var characters = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
 
 test(function (t) {
-	t.assert(alfupperbet.indexOf(characters[Math.floor(Math.random() * characters.length)]) !== -1);
-	t.end();
+  t.assert(alfupperbet.indexOf(characters[Math.floor(Math.random() * characters.length)]) !== -1);
+  t.end();
 });
